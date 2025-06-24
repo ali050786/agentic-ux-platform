325bd3f8efba1e76281e49a57da5ecdca6d430ad
Sikandar Ali <ali050786@gmail.com>
2025-06-24 13:05:43 +0530

    vercel cron

--- vercel-cron-pinger/api/ping.js
+++ vercel-cron-pinger/api/ping.js
@@ -1,3 +1,9 @@
+// /api/ping.js
+
+export const config = {
+  schedule: '*/5 * * * *', // Runs every 5 minutes
+};
+
 export default async function handler(req, res) {
   try {
     const backendURL = 'https://agentic-ux-platform.onrender.com/api/export/public/persona/c422MXo8-9vPRQHX';
